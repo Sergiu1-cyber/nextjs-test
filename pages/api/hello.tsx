f2231475d2cbf6a8d47835fe67e46fd2e4dc4e9c
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2022-12-20 11:51:45 +0200

    api posts

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { title } from "process";
+import { IPosts } from "../../types/IPosts";
+
+const posts: IPosts[] = [
+  {id: 1, title: "Post 1", post: "Blafdksgdfy fvshegfvlsjg fcthjafdey"},
+  {id: 2, title: "Post 2", post: "ygftw yguwyefri jyfdtewdfdruw gfwuykg"},
+  {id: 3, title: "Post 3", post: "uigfe grfre ugfurge gwtgeriwgyt fuewur"}
+]
+
 export default (req, res) => {
-  res.status(200).json({ name: 'Hello, world!' });
+  res.status(200).json({posts});
 };
