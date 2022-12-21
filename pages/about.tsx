ef939f6b90bd35d6e66c8f8160472b901c3ef694
Sergiu1-cyber <68728921+Sergiu1-cyber@users.noreply.github.com>
2022-12-21 12:20:25 +0200

    2 files modified

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,25 +1,20 @@
-import useSWR from 'swr'
-import {IPost} from '../types/IPost';
+import useSWR from 'swr';
+import { IPost } from '../types/IPost';
 
 export default function About() {
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
-  const fetcher = (url: string) => fetch(url).then(r => r.json())
+  const { data: posts, error } = useSWR('/api/posts', fetcher);
 
-  const {data: posts, error} = useSWR('/api/posts', fetcher)
-
-  if(error) {
-    return (
-      <h1>Error</h1>
-    )
+  if (error) {
+    return <h1>Error</h1>;
   }
 
   return (
     <div>
       <h1>Abut Page</h1>
-      {posts && posts.map(
-         (post: IPost) => (<h1 key={post.id}>{post.title}</h1>)
-      )}
+      <h1>Front fetch data with swr</h1>
+      {posts && posts.map((post: IPost) => <h1 key={post.id}>{post.title}</h1>)}
     </div>
   );
 }
-
